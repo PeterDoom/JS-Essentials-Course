0d708b78bde3c638fe38e6493298aa4c8fdae717
Plamen Nanev <48176167+PeterDoom@users.noreply.github.com>
2019-05-13 19:24:51 +0300

    Simple String Manipulation exercise with DOM

---
+++ SecondProblem-Crossword/solution.js
@@ -0,0 +1,70 @@
+function solve() {
+
+   let buttons = document.querySelectorAll('button')
+
+   let output = document.querySelector('#output p')
+
+
+   buttons[0].addEventListener('click', filter);
+   buttons[1].addEventListener('click', sort);
+   buttons[2].addEventListener('click', rotate);
+   buttons[3].addEventListener('click', get);
+
+   
+
+
+   function filter() {
+      let input = document.getElementById('input').value.split('');
+      let secondCmd = document.getElementById('filterSecondaryCmd').value;
+      let position = (+document.getElementById('filterPosition').value) - 1;
+
+      switch (secondCmd) {
+         case 'uppercase':
+            output.innerHTML += input.filter((c) => c === c.toUpperCase())[position]
+            break;
+         case 'lowercase':
+            output.innerHTML += input.filter((c) => c === c.toLowerCase())[position];
+            break;
+         case 'nums':
+            output.innerHTML += input.filter((c) => !isNaN(c))[position];
+            break;
+      }
+
+   }
+
+   function sort() {
+      let input = document.getElementById('input').value.split('').sort((a, b) => a.localeCompare(b));
+      let secondCmd = document.getElementById('sortSecondaryCmd').value;
+      let position = (+document.getElementById('sortPosition').value) - 1;
+
+      if (secondCmd === "Z") {
+         input = input.reverse();
+      }
+      output.innerHTML += input[position]
+
+   }
+
+   function rotate() {
+      let input = document.getElementById('input').value.split('')
+      let secondCmd = document.getElementById('rotateSecondaryCmd').value;
+      let position = (+document.getElementById('rotatePosition').value) - 1;
+
+      let rotation = secondCmd % input.length;
+
+      while (rotation > 0) {
+         let char = input.pop();
+         input.unshift(char);
+         rotation -= 1;
+      }
+
+      output.innerHTML += input[position];
+
+   }
+
+   function get() {
+      let input = document.getElementById('input').value.split('');
+      let position = +document.getElementById('getPosition').value - 1 
+      output.innerHTML += input[position];
+   }
+
+}
